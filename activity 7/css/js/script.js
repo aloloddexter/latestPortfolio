5a59ca17f709113728b6c25fbd6801f42410e74d
letijio02 <101492986+letijio02@users.noreply.github.com>
2024-01-08 22:25:23 +0800

    portfolio

--- activity 7/css/js/script.js
+++
@@ -1,8 +0,0 @@
-const mobileMenuToggle = document.getElementById('mobile-menu');
-const mainMenu = document.getElementById('main-menu');
-
-mobileMenuToggle.addEventListener('click', () => {
-  mainMenu.classList.toggle('active');
-});
-
-
